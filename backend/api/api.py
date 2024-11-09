366a9e9477ab2866ef04ee6527e64be9b19a6bb9
Felix Zhu <felixzhu@Felixs-MacBook-Pro-3.local>
2024-11-09 14:41:28 -0600

    updated for docker

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pyodbc
+import pandas as pd
 
 app = FastAPI()
 
-class ItemResponse(BaseModel):
-    message: str
-    
+db = TicketDB()
 
-@app.get("/")
-def get_data():
-    return{"hello": "world"}
+@app.get("/tickets")
+async def get_tickets():
+    query = "SELECT * FROM TestTable"
+    df = db.load_query_pd(query)
+    # Convert the dataframe to a list of dictionaries to send as JSON
+    return df.to_dict(orient="records")
+
+@app.on_event("shutdown")
+def shutdown():
+    db.close_connection()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
