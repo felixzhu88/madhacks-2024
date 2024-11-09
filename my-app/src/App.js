20db1f729bf8b686c3583c4ab4b7ba7459592626
Lilian4441 <llhuang@wisc.edu>
2024-11-09 14:22:20 -0600

    Changing navbar name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-light"> 
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Ticketer</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
