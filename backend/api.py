00fe1891aa4da6b65ca881b38b4cc9b1b2bf8399
Felix Zhu <felixzhu@Felixs-MacBook-Pro-3.local>
2024-11-09 15:34:49 -0600

    change to api

---
+++ backend/api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from database import TicketDB
+
+app = FastAPI()
+
+db = TicketDB()
+
+@app.get("/tickets")
+async def get_tickets():
+    query = "SELECT * FROM TestTable"
+    df = db.load_query_pd(query)
+    # Convert the dataframe to a list of dictionaries to send as JSON
+    return df.to_JSON(orient="records")
+
+@app.on_event("shutdown")
+def shutdown():
+    db.close_connection()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
